4653c55b0a6cdee36ae2d0c128932cf352324e23
Akash verma <42277681+lusifer65@users.noreply.github.com>
2020-10-14 13:43:22 -0400

    Create ConvertHoursAndMinutesIntoSeconds.py

---
+++ Solutions/ConvertHoursAndMinutesIntoSeconds.py
@@ -0,0 +1,7 @@
+'''
+Problem statement: Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+Problem Link: https://edabit.com/challenge/PjcKZRx8YE5KzRN63
+'''
+
+def convert(h,m):
+    return (h*60+m)*60
